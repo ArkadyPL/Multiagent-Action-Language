f7231e74d5a6d88ba852dd371549f63a339d0390
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-05-04 18:44:04 +0200

    Add more statements to exemplary model

--- model.pl
+++ model.pl
@@ -1,7 +1,21 @@
-after(alpha, [[a1, [g1, g2]], [a2, [g3, g4]]]).
-after(alpha, [[a1, [g1, g3]], [a2, [g2]]]).
-after(beta, [[a1, [g1]]]):- true, !.
+after(alpha, [[action1, [g1, g2]], [action2, [g3, g4]]]).
+after(alpha, [[action1, [g1, g3]], [action2, [g2]]]).
+after(beta, [[action1, [g1]]]):- true, !.
 
-observable_after(alpha, [[a1, [g1]], [a2, [g2]]]).
+
+observable_after(alpha, [[action1, [g1]], [action2, [g2]]]).
+
 
 initially(beta).
+
+
+causes_by_if(action1, [g1], alpha, pi).
+
+causes_by(action1, [g1], alpha).
+
+causes_if(action1, alpha, pi).
+
+
+impossible_by_if(action1, [g1], pi).
+
+impossible_if(action1, pi).
