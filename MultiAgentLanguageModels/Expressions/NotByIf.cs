dd512040d7da60085bae8ddc542a6cdd89c06e40
Piotr Konowrocki <p.konowrocki@samsung.com>
2019-05-27 08:30:13 +0200

    Not by if expression added

---
+++ MultiAgentLanguageModels/Expressions/NotByIf.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace MultiAgentLanguageModels.Expressions
+{
+    public class NotByIf : Expression
+    {
+        public Action Action { get; }
+        public AgentsList Agents { get; }
+        LogicExpression Condition { get; }
+        public NotByIf(Action action, AgentsList agents, LogicExpression condition)
+        {
+            Action = action;
+            Agents = agents;
+            Condition = condition;
+        }
+        public override string ToProlog()
+        {
+            return Agents.Select(a => new AgentsList() { a })
+                .Select(al => new ImpossibleByIf(Action, al, Condition))
+                .Select(impossible => impossible.ToProlog())
+                .Aggregate((a, b) => a + "\n" + b);
+        }
+    }
+
+    public class NotBy : NotByIf
+    {
+        public NotBy(Action action, AgentsList agents)
+            : base(action, agents, new True())
+        {
+        }
+
+        public override string ToProlog()
+        {
+            return Agents.Select(a => new AgentsList() { a })
+                .Select(al => new ImpossibleBy(Action, al))
+                .Select(impossible => impossible.ToProlog())
+                .Aggregate((a, b) => a + "\n" + b);
+        }
+    }
+}
