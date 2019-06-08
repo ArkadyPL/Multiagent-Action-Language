df7076c43f0e4c74064484daef48aaf518fbdb87
pkonowrocki <piotrek.konowrocki@gmail.com>
2019-06-08 15:59:19 +0200

    NecessaryAfter fix, PossiblyAfter done

---
+++ ModelsTests/TestPossiblyAfter.cs
@@ -0,0 +1,98 @@
+﻿using MultiAgentLanguageGUI;
+using MultiAgentLanguageModels.Expressions;
+using MultiAgentLanguageModels.Queries;
+using NUnit.Framework;
+
+namespace MultiAgentLanguageModelsTests
+{
+    public class TestPossiblyAfter
+    {
+        [Test]
+        public void Test1()
+        {
+            string story = @"
+Fluent hasA
+Fluent hasB
+Agent g
+Action buypaper
+buypaper by [g] causes [hasA || hasB]
+";
+            var tokens = Tokenizer.Tokenize(story);
+            var parserState = Parser.Parse(tokens);
+            var expressions = new ExpressionsList();
+            expressions.AddRange(parserState.Expression);
+            expressions.AddRange(parserState.Noninertial.Values);
+
+            string query = @"
+possibly [hasA] after (buypaper, [g])
+";
+
+            Query q = Parser.ParseQuerry(
+                Tokenizer.Tokenize(query),
+                parserState);
+
+            var res = q.Solve(expressions);
+
+            Assert.AreEqual(false, res);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string story = @"
+Fluent hasA
+Fluent hasB
+Agent g
+Action buypaper
+initially [hasA]
+buypaper by [g] causes [hasA || hasB]
+";
+            var tokens = Tokenizer.Tokenize(story);
+            var parserState = Parser.Parse(tokens);
+            var expressions = new ExpressionsList();
+            expressions.AddRange(parserState.Expression);
+            expressions.AddRange(parserState.Noninertial.Values);
+
+            string query = @"
+possibly [hasA] after (buypaper, [g])
+";
+
+            Query q = Parser.ParseQuerry(
+                Tokenizer.Tokenize(query),
+                parserState);
+
+            var res = q.Solve(expressions);
+
+            Assert.AreEqual(true, res);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string story = @"
+Fluent hasA
+Fluent hasB
+Agent g
+Action buypaper
+buypaper by [g] causes [hasA || hasB]
+";
+            var tokens = Tokenizer.Tokenize(story);
+            var parserState = Parser.Parse(tokens);
+            var expressions = new ExpressionsList();
+            expressions.AddRange(parserState.Expression);
+            expressions.AddRange(parserState.Noninertial.Values);
+
+            string query = @"
+possibly [hasA || hasB] after (buypaper, [g])
+";
+
+            Query q = Parser.ParseQuerry(
+                Tokenizer.Tokenize(query),
+                parserState);
+
+            var res = q.Solve(expressions);
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
--- MultiAgentLanguageModels/Queries/PossiblyAfterFrom.cs
+++ MultiAgentLanguageModels/Queries/PossiblyAfterFrom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using MultiAgentLanguageModels.Expressions;
+using MultiAgentLanguageModels.Reasoning;
 
 namespace MultiAgentLanguageModels.Queries
 {
@@ -19,7 +20,69 @@ namespace MultiAgentLanguageModels.Queries
 
         public override bool Solve(ExpressionsList expressions)
         {
-            throw new System.NotImplementedException();
+            var reasoningEngine = new ReasoningEngine();
+            var res = reasoningEngine.Res(expressions);
+            var initialStates = reasoningEngine.InitialStates(expressions);
+            var allStates = reasoningEngine.PossibleStates(expressions);
+            var piCondition = Condition.EvaluateLogicExpression();
+            var alphaCondition = Result.EvaluateLogicExpression();
+
+            //we want that list to hold result of query for each initial state
+            //could be done prettier but it's easier to read
+            List<bool> resultsForEachInitiallState = new List<bool>();
+
+            //for each initiall state
+            foreach (var initialState in initialStates)
+            {
+                HashSet<State> currentStates = new HashSet<State>();
+                //if condition is always true then our current state is initial state
+                if (Condition.Element is True)
+                {
+                    currentStates.Add(initialState);
+                }
+                //else we have to find all states that are ok
+                else
+                {
+                    foreach (var state in allStates)
+                    {
+                        if (piCondition.Any(x => state.Values.HasSubset(x)))
+                        {
+                            currentStates.Add(state);
+                        }
+                    }
+                }
+                //now we iterate through instructions
+                for (int i = 0; i < Instructions.Count; i++)
+                {
+                    var action = Instructions[i].Item1;
+                    var agents = Instructions[i].Item2;
+                    HashSet<State> newCurrentStates = new HashSet<State>();
+                    //for each state in current states we want to move forward in graph
+                    foreach (var currentState in currentStates)
+                    {
+                        var triple = new Triple(action, currentState, agents);
+                        //if we can find good edge in graph 
+                        //from currentState, specific action and agents group then
+                        if (res.ContainsKey(triple))
+                        {
+                            //add all next states to the newCurrentStates
+                            res[triple].ToList().ForEach(s => newCurrentStates.Add(s));
+                        }
+                    }
+                    //do it again for new action and agents group
+                    currentStates = newCurrentStates;
+                }
+                //finally we are in the last nodes after the whole instruction
+                //for each initial state we want see if any of states (last nodes) are compliant with alpha
+                //therefore we need to add result to resultsForEachInitiallState list
+                resultsForEachInitiallState.Add(
+                    currentStates
+                    .Any(finallState => alphaCondition
+                                        .Any(aCon => finallState.Values.HasSubset(aCon))
+                    )
+                );
+            }
+            return resultsForEachInitiallState.All(x => x);
         }
     }
     public class PossiblyAfter : PossiblyAfterFrom
