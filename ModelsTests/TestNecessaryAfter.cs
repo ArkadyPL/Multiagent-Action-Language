12bd32060714ef70e7f4b1e79bc6e8a81aa3e02d
pkonowrocki <piotrek.konowrocki@gmail.com>
2019-06-08 20:56:07 +0200

    non-deterministic tests

--- ModelsTests/TestNecessaryAfter.cs
+++ ModelsTests/TestNecessaryAfter.cs
@@ -64,5 +64,69 @@ necessary [hasA || hasB] after (buypaper, [g])
 
             Assert.AreEqual(true, res);
         }
+
+        [Test]
+        public void Test3()
+        {
+            string story = @"
+Fluent hasA
+Fluent hasB
+Agent g
+Action buypaper
+buypaper by [g] causes [hasA || hasB]
+initially [hasB]
+buypaper by [g] releases [hasA]
+buypaper by [g] releases [hasB]
+";
+            var tokens = Tokenizer.Tokenize(story);
+            var parserState = Parser.Parse(tokens);
+            var expressions = new ExpressionsList();
+            expressions.AddRange(parserState.Expression);
+            expressions.AddRange(parserState.Noninertial.Values);
+
+            string query = @"
+necessary [hasA] after (buypaper, [g])
+";
+
+            Query q = Parser.ParseQuerry(
+                Tokenizer.Tokenize(query),
+                parserState);
+
+            var res = q.Solve(expressions);
+
+            Assert.AreEqual(false, res);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            string story = @"
+Fluent hasA
+Fluent hasB
+Agent g
+Action buypaper
+buypaper by [g] causes [hasA || hasB]
+initially [hasB]
+buypaper by [g] releases [hasA]
+buypaper by [g] releases [hasB]
+";
+            var tokens = Tokenizer.Tokenize(story);
+            var parserState = Parser.Parse(tokens);
+            var expressions = new ExpressionsList();
+            expressions.AddRange(parserState.Expression);
+            expressions.AddRange(parserState.Noninertial.Values);
+
+            string query = @"
+necessary [hasA || hasB] after (buypaper, [g])
+";
+
+            Query q = Parser.ParseQuerry(
+                Tokenizer.Tokenize(query),
+                parserState);
+
+            var res = q.Solve(expressions);
+
+            Assert.AreEqual(true, res);
+        }
     }
 }
