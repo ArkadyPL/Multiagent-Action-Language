ed88cd0e0da98eeb6e45971f9a3cce4c4398cc7b
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-06-09 01:46:41 +0200

    unify tests' naming convention

--- ModelsTests/Reasoning/PossibleStatesTest.cs
+++ ModelsTests/Reasoning/PossibleStatesTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using MultiAgentLanguageModels;
 using System.Collections.Generic;
 
-namespace MultiAgentLanguageModelsTests
+namespace MultiAgentLanguageModelsTests.Reasoning
 {
     public class PossibleStatesTests
     {
