6870ce3f7dd2971d67dd16e2f28ccc324a426fe1
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-06-08 17:25:11 +0200

    initial version of PossiblyExecutableFrom + test

--- MultiAgentLanguageModels/Queries/PossiblyExecutableFrom.cs
+++ MultiAgentLanguageModels/Queries/PossiblyExecutableFrom.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using MultiAgentLanguageModels;
 using MultiAgentLanguageModels.Expressions;
+using MultiAgentLanguageModels.Queries;
+using MultiAgentLanguageModels.Reasoning;
 
 namespace MultiAgentLanguageModels.Queries
 {
@@ -16,7 +19,56 @@ namespace MultiAgentLanguageModels.Queries
 
         public override bool Solve(ExpressionsList expressions)
         {
-            throw new System.NotImplementedException();
+            var reasoningEngine = new ReasoningEngine();
+            var res = reasoningEngine.Res(expressions);
+            var initialStates = reasoningEngine.InitialStates(expressions);
+            var allStates = reasoningEngine.PossibleStates(expressions);
+            var piCondition = Condition.EvaluateLogicExpression();
+            
+            //for each initiall state
+            foreach (var initialState in initialStates)
+            {
+                HashSet<State> currentStates = new HashSet<State>();
+                //if condition is always true then our current state is initial state
+                if (Condition.Element is True)
+                {
+                    currentStates.Add(initialState);
+                }
+                //else we have to find all states that are ok
+                else
+                {
+                    foreach (var state in allStates)
+                    {
+                        if (piCondition.Any(x => state.Values.HasSubset(x)))
+                        {
+                            currentStates.Add(state);
+                        }
+                    }
+                }
+                //now we iterate through instructions
+                for (int i = 0; i < Instructions.Count; i++)
+                {
+                    var action = Instructions[i].Item1;
+                    var agents = Instructions[i].Item2;
+                    HashSet<State> newCurrentStates = new HashSet<State>();
+                    //for each state in current states we want to move forward in graph
+                    foreach (var currentState in currentStates)
+                    {
+                        var triple = new Triple(action, currentState, agents);
+                        //if we can find good edge in graph 
+                        //from currentState, specific action and agents group then
+                        if (res.ContainsKey(triple))
+                        {
+                            //add all next states to the newCurrentStates
+                            res[triple].ToList().ForEach(s => newCurrentStates.Add(s));
+                        }
+                    }
+                    if (newCurrentStates.Count == 0) return false;
+                    //do it again for new action and agents group
+                    currentStates = newCurrentStates;
+                }
+            }
+            return true;
         }
     }
 
