26d80a886a0f86e9222fa962aac0550ed830bc3f
pkonowrocki <piotrek.konowrocki@gmail.com>
2019-06-09 19:53:02 +0200

    AlchemyTest fix

--- ModelsTests/ForOtherTeam/AlchemyTest.cs
+++ ModelsTests/ForOtherTeam/AlchemyTest.cs
@@ -46,8 +46,11 @@ namespace MultiAgentLanguageModelsTests.ForOtherTeam
             sb.AppendLine(@"
             brew by [Bercik, Filemon] causes [brewed]
             brew by [Filemon] releases [brewed]
+            brew by [Filemon] causes [brewed || ~brewed]
             brew by [Bercik] releases [brewed]
-            brew by [Bercik] releases [destroyed]");
+            brew by [Bercik] causes [brewed || ~brewed]
+            brew by [Bercik] releases [destroyed]
+            brew by [Bercik] causes [destroyed || ~destroyed]");
 
             string story = sb.ToString();
             var tokens = Tokenizer.Tokenize(story);
